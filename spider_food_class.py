4ae843b374e02fd497de6218adb11c154b4116ae
lanboys <lan_bing2013@163.com>
2018-12-07 19:01:39 +0800

    爬取菜品材料列表数据

--- spider_food_class.py
+++ spider_food_class.py
@@ -94,20 +94,37 @@ def loadFoodPages():
 
 def loadFoodList():
     sql = "select id, parent_id, url, total_page, current_page  from lb_food_class where level = %s " \
-          "and total_page > current_page and parent_id not in (%s, %s)"
-    params = (2, 6, 7)
+          "and total_page > current_page and parent_id not in (6, 7)"
+    loadFoodListComm(sql, 1)
+
+
+def loadFoodMaterialList():
+    sql = "select id, parent_id, url, total_page, current_page  from lb_food_class where level = %s " \
+          "and total_page > current_page and parent_id in (7, 7)"
+    loadFoodListComm(sql, 2)
+
+
+def loadFoodListComm(sql, type):
+    params = (2,)
     print(sql % params)
     rows = MysqlHelper.MysqlHelper().fetchall(sql, params)
 
     for row in rows:
         id, parent_id, url, total_page, current_page = row
 
+        # total_page = total_page - 30
+        # if total_page < current_page:
+        #     total_page = current_page + 5
+
         for page in range(current_page + 1, total_page + 1):
             page_ = "?&page="
             if "?" in url:
                 page_ = "&page="
             # 爬取每页数据
-            loadFoodListPage(url + page_ + str(page), id, parent_id)
+            if type == 1:
+                loadFoodListPage(url + page_ + str(page), id, parent_id)
+            else:
+                loadFoodMaterialListPage(url + page_ + str(page), id, parent_id)
             # 更新页码
             updateCurrentPage(id, page)
             # 休眠1秒
@@ -153,6 +170,24 @@ def loadFoodListPage(url, class1_id, class2_id):
         MysqlHelper.MysqlHelper().cud(sql, params)
 
 
+def loadFoodMaterialListPage(url, class1_id, class2_id):
+    print (url)
+    divList = getContent(url).xpath('//div[@class="listtyle1"]')
+    for div in divList:
+        href = div.xpath('div[@class="img"]/a/@href')[0]
+        logoUrl = div.xpath("div/a/img/@src")[0]
+        title = div.xpath('div[@class="info1"]/h3/a/text()')[0]
+        description = ""
+        descriptionList = div.xpath('div[@class="info1"]/div/span/text()')
+        for d in descriptionList:
+            description = d
+
+        sql = "insert into lb_food_material ( name , class1_id , class2_id ,description, url , logo_url) values ( %s, %s, %s, %s, %s, %s)"
+        params = (title, class1_id, class2_id, description, href, logoUrl)
+        # print(sql % params)
+        MysqlHelper.MysqlHelper().cud(sql, params)
+
+
 if __name__ == "__main__":
     # 爬取第一分类
     # loadFoodClass1()
@@ -164,4 +199,7 @@ if __name__ == "__main__":
     # loadFoodPages()
 
     # 爬取第二分类下菜品列表信息
-    loadFoodList()
+    # loadFoodList()
+
+    # 爬取第二分类下菜品材料列表信息
+    loadFoodMaterialList()
